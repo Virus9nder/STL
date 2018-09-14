f3f17ded67e5da6e87929e315c61e90340364fda
Virus <sashka-k@inbox.ru>
2018-09-14 16:30:47 +0300

    Correct name of class

--- git/Calc v 0.01/ConsoleApp1/Program.cs
+++ git/Calc v 0.01/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    public static class F
+    public static class Calc
     {
         private static string check = "0123456789,.";
         public static double a;
@@ -17,11 +17,11 @@ namespace ConsoleApp1
             int ui = 0;
             foreach (var item in x)
             {
-                foreach (var item2 in F.check)
+                foreach (var item2 in Calc.check)
                 {
                     if (item == item2) ui = 1;
                 }
-                if (ui == 0) { Console.WriteLine("Введены некорректные данные, повторите ввод:"); x = Console.ReadLine(); return F.Do(x);  }
+                if (ui == 0) { Console.WriteLine("Введены некорректные данные, повторите ввод:"); x = Console.ReadLine(); return Calc.Do(x);  }
             }
             string cc2 = "";
             foreach (var item in x)
@@ -34,23 +34,23 @@ namespace ConsoleApp1
         }
         public static void Add()
         {
-            
-            F.c= F.a+F.b;
+
+            Calc.c= Calc.a+ Calc.b;
         }
         public static void Sub()
         {
 
-            F.c = F.a - F.b;
+            Calc.c = Calc.a - Calc.b;
         }
         public static void Div()
         {
 
-            F.c = F.a / F.b;
+            Calc.c = Calc.a / Calc.b;
         }
         public static void Mull()
         {
 
-            F.c = F.a * F.b;
+            Calc.c = Calc.a * Calc.b;
         }
 
 
@@ -69,23 +69,23 @@ namespace ConsoleApp1
 
                 string cc = Console.ReadLine();
 
-                F.a = F.Do(cc);
+                Calc.a = Calc.Do(cc);
                 Console.WriteLine("Введите B:");
 
                 cc = Console.ReadLine();
 
-                F.b = F.Do(cc);
+                Calc.b = Calc.Do(cc);
                 Console.WriteLine("Выберите операцию:\n1-Сложение\n2-Вычитание\n3-Деление\n4-Умножение");
                 int1= int.Parse(Console.ReadLine());
                 switch (int1)
                 {
-                    case 1: F.Add(); break;
-                    case 2: F.Sub(); break;
-                    case 3: F.Div(); break;
-                    case 4: F.Mull(); break;
+                    case 1: Calc.Add(); break;
+                    case 2: Calc.Sub(); break;
+                    case 3: Calc.Div(); break;
+                    case 4: Calc.Mull(); break;
                     default: Console.WriteLine("Введите корректные данные"); break;
                 }
-                Console.WriteLine("Ответ: "+F.c);
+                Console.WriteLine("Ответ: "+ Calc.c);
             }
 
         }
