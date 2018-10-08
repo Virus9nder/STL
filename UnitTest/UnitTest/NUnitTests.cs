54daf4cb747b85e962011cd89539d4d5995f21aa
Virus <sashka-k@inbox.ru>
2018-10-08 18:49:23 +0300

    UnitTest v1.2

--- UnitTest/UnitTest/NUnitTests.cs
+++ UnitTest/UnitTest/NUnitTests.cs
@@ -6,61 +6,61 @@ namespace UnitTest
     [TestFixture]
     class NUnitTests
     {
-        [TestCase]
+        [Test]
         public void GeneralTest()
         {
             Assert.IsTrue(Figure.isTriangle(1, 1, 1));
         }
 
-        [TestCase]
+        [Test]
         public void ZeroTest()
         {
             Assert.IsFalse(Figure.isTriangle(1, 0, 1));
         }
 
-        [TestCase]
+        [Test]
         public void MinusTest()
         {
             Assert.IsFalse(Figure.isTriangle(-1, 1, 1));
         }
 
-        [TestCase]
+        [Test]
         public void ConvertTest()
         {
             Assert.IsTrue(Figure.isTriangle(Convert.ToDouble("1,1"), 1, 1));
         }
 
-        [TestCase]
+        [Test]
         public void BoundaryTest()
         {
             Assert.IsFalse(Figure.isTriangle(0.00000000000000000000000000000000000000000000000001, 1, 1));
         }
 
-        [TestCase]
+        [Test]
         public void MaxMinValueTest()
         {
             Assert.IsFalse(Figure.isTriangle(double.MaxValue, 2 * double.MinValue, double.MaxValue));
         }
 
-        [TestCase]
+        [Test]
         public void MaxValueTest()
         {
             Assert.IsTrue(Figure.isTriangle(double.MaxValue, double.MaxValue, double.MaxValue));
         }
 
-        [TestCase]
+        [Test]
         public void UpperMaxValueTest()
         {
             Assert.IsFalse(Figure.isTriangle(double.MaxValue + double.MinValue, double.MaxValue + double.MinValue, double.MaxValue + double.MinValue));
         }
 
-        [TestCase]
+        [Test]
         public void BoundaryLowerTest()
         {
             Assert.IsTrue(Figure.isTriangle(1 + 0.000000011, 2, 3 + 0.00000001));
         }
 
-        [TestCase]
+        [Test]
         public void BoundaryUpperTest()
         {
             Assert.IsFalse(Figure.isTriangle(1, 2, 3 + 0.00000001));
