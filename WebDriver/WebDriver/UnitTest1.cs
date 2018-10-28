04321b067036f8b5a7e219d99f153a49f0a0cd92
Virus <sashka-k@inbox.ru>
2018-10-28 16:43:50 +0300

    WebDriver v2.0

--- WebDriver/WebDriver/UnitTest1.cs
+++ WebDriver/WebDriver/UnitTest1.cs
@@ -10,45 +10,27 @@ namespace WebDriver
     [TestClass]
     public class WebDriverTest
     {
+       
         [TestMethod]
-        public void FirsWebDrvertTest()
-        {
-            //RemoteWebDriver Driver = new ChromeDriver();
-            //RemoteWebDriver Driver = new InternetExplorerDriver();
-            RemoteWebDriver Driver = new InternetExplorerDriver("D:\\IEDriverServer.exe");
-            Driver.Navigate().GoToUrl(@"http://www.vento.by/avia");
-
-            var Вeparture = Driver.FindElementById("Departure_1_label");
-            var Arrival = Driver.FindElementById("Arrival_1_label");
-            var ВepartureCalendar = Driver.FindElementById("CalendarLink1");
-            var ArrivalCalendar = Driver.FindElementById("CalendarLinkReturn");
-            var Confirm = Driver.FindElementById("searchBtn");
-
-            Вeparture.Clear();
-            Вeparture.SendKeys("Минск");
-            Arrival.Clear();
-            Arrival.SendKeys("Москва");
-            Arrival.Clear();
-            ВepartureCalendar.Clear();
-            ВepartureCalendar.SendKeys("26/10");
-            ArrivalCalendar.Clear();
-            ArrivalCalendar.SendKeys("27/10");
-
-            Confirm.Click();
-        }
-
-        [TestMethod]
-        public void NewWebDriverTest()
+        public void LastWebDriverTest()
         {
             IWebDriver Driver = new ChromeDriver();
-            Driver.Navigate().GoToUrl("http://www.vento.by/avia");
-            Driver.FindElement(By.Id("Departure_1_label")).SendKeys("Минск");
-           // Driver.FindElement(By.Id("Arrival_1_label")).SendKeys("Москва");
-            Driver.FindElement(By.Id("CalendarLink1")).SendKeys("26/10");
-            Driver.FindElement(By.Id("CalendarLinkReturn")).SendKeys("27/10");
-            Driver.FindElement(By.Id("searchBtn")).Click();
+            Driver.Manage().Window.Size = new System.Drawing.Size(600, 1000);
+            Driver.Navigate().GoToUrl("https://www.chocotravel.com/");
+            System.Threading.Thread.Sleep(10000);
+            Driver.FindElement(By.Id("city_1_user")).SendKeys("Минск" + Keys.Enter);
+            Driver.FindElement(By.Id("city_2_user")).SendKeys("Москва" + Keys.Enter);
+            Driver.FindElement(By.Id("fir_date_mobile")).SendKeys("2018-11-11" + Keys.Enter);
+            //  Driver.FindElement(By.Id("sec_date_mobile")).SendKeys("2018-11-12" + Keys.Enter);
+            Driver.FindElement(By.ClassName("search-form__button_search__mobile")).Click();
+            Assert.AreEqual(Driver.FindElement(By.ClassName("dialog-content")).Text, "Выбраны неверные даты");
+            Driver.Close();
             Driver.Quit();
+
+
+
         }
+
     }
 }
 
