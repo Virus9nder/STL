2fc84671ae1f2746f42e4b3372c1db985bdb0529
Virus <sashka-k@inbox.ru>
2018-12-02 13:28:11 +0300

    WebDriverATF2 v0.03

---
+++ WebDriverATF2/WebDriverATF2/Pages/BookingPage.cs
@@ -0,0 +1,87 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace WebDriverATF2.Pages
+{
+    class BookingPage
+    {
+        private readonly IWebDriver driver;
+        private readonly WebDriverWait wait;
+       
+        [FindsBy(How = How.Id, Using = "cheapest_price")]
+        private IWebElement bookingSite;
+
+        [FindsBy(How = How.ClassName, Using = "book_surname")]
+        private IWebElement surname;
+
+        [FindsBy(How = How.ClassName, Using = "book_surname")]
+        private IWebElement name;
+
+        [FindsBy(How = How.ClassName, Using = "book_birth_date")]
+        private IWebElement birthDate;
+
+        [FindsBy(How = How.ClassName, Using = "book_country")]
+        private IWebElement passportCountry;
+
+        [FindsBy(How = How.ClassName, Using = "book_number")]
+        private IWebElement passportNumber;
+
+        [FindsBy(How = How.ClassName, Using = "book_doc_exp_date")]
+        private IWebElement passportDate;
+
+        [FindsBy(How = How.ClassName, Using = "boy")]
+        private IWebElement genderBoy;
+
+        [FindsBy(How = How.Id, Using = "country")]
+        private IWebElement country;
+
+        [FindsBy(How = How.ClassName, Using = "book_phone")]
+        private IWebElement phone;
+
+        [FindsBy(How = How.ClassName, Using = "book_email")]
+        private IWebElement email;
+
+        [FindsBy(How = How.Id, Using = "al1")]
+        private IWebElement bookingAgree;
+
+        [FindsBy(How = How.Id, Using = "booking_button")]
+        private IWebElement bookingButton;
+
+        [FindsBy(How = How.Id, Using = "tooltipster-content")]
+        private IWebElement errorMessage;
+
+        public BookingPage(IWebDriver Browser)
+        {
+            this.driver = Browser;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            PageFactory.InitElements(Browser, this);
+        }
+
+        public IWebElement GetErrorMessage()
+        {
+            return errorMessage;
+        }
+                
+        public void NewBookingData(BookingData bookingData)
+        {
+            wait.Until(ExpectedConditions.ElementToBeClickable(bookingSite));
+            bookingSite.Click();
+            wait.Until(ExpectedConditions.ElementToBeClickable(surname));
+            surname.SendKeys(bookingData.surname + Keys.Enter);
+            name.SendKeys(bookingData.name + Keys.Enter);
+            birthDate.SendKeys(bookingData.birthDate + Keys.Enter);
+            passportCountry.SendKeys(bookingData.passportCountry + Keys.Enter);
+            passportNumber.SendKeys(bookingData.passportNumber + Keys.Enter);
+            passportDate.SendKeys(bookingData.passportDate + Keys.Enter);
+            genderBoy.Click();
+            country.SendKeys(bookingData.country + Keys.Enter);
+            phone.SendKeys(bookingData.phone + Keys.Enter);
+            email.SendKeys(bookingData.email + Keys.Enter);
+            bookingAgree.Click();
+            bookingButton.Click();
+        }
+    }
+}
+
---
+++ WebDriverATF2/WebDriverATF2/Pages/MainPage.cs
@@ -0,0 +1,68 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace WebDriverATF2.Pages
+{
+    class MainPage
+    {
+        private readonly IWebDriver driver;
+        private readonly WebDriverWait wait;
+        private readonly string url = "https://www.chocotravel.com/";
+
+        [FindsBy(How = How.Id, Using = "city_1_user")]
+        private IWebElement cityFrom;
+
+        [FindsBy(How = How.Id, Using = "city_2_user")]
+        private IWebElement cityTo;
+
+        [FindsBy(How = How.Id, Using = "fir_date_mobile")]
+        private IWebElement dateFrom;
+
+        [FindsBy(How = How.Id, Using = "sec_date_mobile")]
+        private IWebElement dateTo;
+
+        [FindsBy(How = How.ClassName, Using = "psngrs_type")]
+        private IWebElement addPerson;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='search_form']/div/div[2]/div[9]/div[6]/div/ul[2]/li[4]/div[3]")]
+        private IWebElement addBaby;
+
+        [FindsBy(How = How.ClassName, Using = "search-form__button_search__mobile")]
+        private IWebElement buttonSearch;
+
+        [FindsBy(How = How.ClassName, Using = "dialog-content")]
+        private IWebElement errorMessage;
+
+        public MainPage(IWebDriver Browser)
+        {
+            this.driver = Browser;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            this.driver.Manage().Window.Size = new System.Drawing.Size(driver.Manage().Window.Size.Width / 2, driver.Manage().Window.Size.Height);
+            PageFactory.InitElements(Browser, this);
+        }
+        
+        public IWebElement GetErrorMessage()
+        {
+            return errorMessage;
+        }
+
+        public void NewMainData(string CityFrom, string CityTo, string DateFrom, string DateTo, int BabyCount)
+        {
+            driver.Navigate().GoToUrl(this.url);
+            wait.Until(ExpectedConditions.ElementToBeClickable(cityFrom));
+            cityFrom.SendKeys(CityFrom + Keys.Enter);
+            cityTo.SendKeys(CityTo + Keys.Enter);
+            dateFrom.SendKeys(DateFrom + Keys.Enter);
+            dateTo.SendKeys(DateTo + Keys.Enter);
+            addPerson.Click();
+            for (int i = 0; i < BabyCount; i++)
+            {
+                addBaby.Click();
+            }
+            buttonSearch.Click();
+        }
+    }
+}
+
